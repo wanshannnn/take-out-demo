74e2fa0a8dd89d06f593093ae3d7bea13913c4f9
wanshannnn <3179157197@qq.com>
2024-10-17 16:23:13 +0800

    init

---
+++ src/test/java/com/demo/skytakeoutdemo/SkyTakeOutDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.skytakeoutdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyTakeOutDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
